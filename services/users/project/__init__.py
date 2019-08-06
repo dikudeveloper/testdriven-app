c0c83fdb0aeeea79b3f956ccf6572d9dc55c466e
Joseph Diku <joseph.diku@gmail.com>
2019-08-06 20:53:28 +0300

    Add APP_SETTINGS environment variable for Development environment

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,4 +1,6 @@
 
+import os
+
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
 
@@ -10,6 +12,8 @@ api = Api(app)
 
 # set config
 app.config.from_object('project.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
 
 
 class UsersPing(Resource):
