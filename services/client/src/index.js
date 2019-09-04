57ec23c69ee40b06fc6197f1b4c2d32d48c6a1e3
Joseph Diku <joseph.diku@gmail.com>
2019-09-04 20:09:42 +0300

    Class based component

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, { component } from 'react';
 import ReactDOM from 'react-dom';
 
 
-const App = () => {
-  return (
-    <section className="section">
-      <div className="container">
-        <div className="columns">
-          <div className="column is-one-third">
-            <br/>
-            <h1 className="title is-1 is-1">All Users</h1>
-            <hr/><br/>
+class App extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <section className="section">
+        <div className="container">
+          <div className="columns">
+            <div className="column is-one-third">
+              <br/>
+              <h1 className="title is-1">All Users</h1>
+              <hr/><br/>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
-  )
+      </section>
+    )
+  }
 };
-
 
 ReactDOM.render(
   <App />,
