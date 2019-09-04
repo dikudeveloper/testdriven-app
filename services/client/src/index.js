933f381143a01956fac5e88d5e2e308543777094
Joseph Diku <joseph.diku@gmail.com>
2019-09-04 21:34:00 +0300

    Add Axios to make XmlHttpRequests from client service to users backend service

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -1,10 +1,18 @@
-import React, { component } from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+
+import axios from 'axios';
 
 
 class App extends Component {
   constructor() {
     super();
+    this.getUsers()
+  }
+  getUsers() {
+    axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
+    .then((res) => { console.log(res); })
+    .catch((err) => { console.log(err); });
   }
   render() {
     return (
