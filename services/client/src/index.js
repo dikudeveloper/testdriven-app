594d2abc29a49b93f75a4ec11053739e1bb447b1
Joseph Diku <joseph.diku@gmail.com>
2019-09-04 22:36:56 +0300

    Render the state

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -31,6 +31,16 @@ class App extends Component {
               <br/>
               <h1 className="title is-1">All Users</h1>
               <hr/><br/>
+              {
+                this.state.users.map((user) => {
+                  return (
+                    <h4
+                      key={user.id}
+                      className="box title is-4">{ user.username }
+                    </h4>
+                  )
+                })
+              }
             </div>
           </div>
         </div>
